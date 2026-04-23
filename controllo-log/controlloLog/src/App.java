853d5cd7d9269d7a4bf5700934e9dfe1557852c4
Fede-46 <federico.agostini@marconirovereto.it>
2026-04-23 06:34:41 +0000

    Fix: main App

--- controllo-log/controlloLog/src/App.java
+++ controllo-log/controlloLog/src/App.java
@@ -1,13 +1,7 @@
-import controllo_log.Logs;
-import controllo_log.TipoOperazione;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        Logs l1 = new Logs();
-        Logs l2 = new Logs(5000);
-        Logs l3 = new Logs(10000, 0.5);
-        l1.caricaFile(null, false, null, null);
-        
+        Comune c = new Comune("PD;\"ABANO TERME\";A001;14/11/1924;");
+        System.out.println(c.toString());
     }
 }
